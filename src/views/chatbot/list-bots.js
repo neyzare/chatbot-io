3e7e3b28d496ef3893d5e4654b2787c90e6d8c5d
TaliorTheRogue <donribeiro45@gmail.com>
2024-02-20 14:32:03 +0100

    Fixed botlist overflow and scroll

--- src/views/chatbot/list-bots.js
+++ src/views/chatbot/list-bots.js
@@ -1,8 +1,15 @@
 import viewBot from './bot';
 
 export default () => (`
-<div class="col-3 border-end border-5 pe-4 bot-list">
+<div class="col-3 border-end border-5 pe-4 overflow-auto bot-list">
   <ul class="list-group">
+    ${viewBot()}
+    ${viewBot()}
+    ${viewBot()}
+    ${viewBot()}
+    ${viewBot()}
+    ${viewBot()}
+    ${viewBot()}
     ${viewBot()}
     ${viewBot()}
     ${viewBot()}
