45a5ee36ce2df0326644b01f5b5033f5c47929fd
TaliorTheRogue <donribeiro45@gmail.com>
2024-02-22 09:11:56 +0100

    Add view/message to appear in list

---
+++ src/views/chatbot/list-messages.js
@@ -0,0 +1,13 @@
+import viewMessage from './message';
+
+export default () => (`
+  <div class="col-9 chat">
+    <div class="container-fluid overflow-auto chat-window">
+      ${viewMessage()}
+    </div>
+    <div class="input-group fixed mt-3 ms-4 pe-5">
+      <input type="text" class="form-control border border-3 rounded me-2" placeholder="/Type your message" aria-label="Message" aria-describedby="button-addon2">
+      <button class="btn btn-outline-dark rounded-circle border border-3 send" type="button" id="button-addon2"><i class="fa-solid fa-paper-plane"></i></button>
+    </div>
+  </div>
+`);
---
+++ src/views/chatbot/message.js
@@ -0,0 +1,19 @@
+export default () => (`
+<div class="row mb-4">
+  <div class="col-6"></div>
+  <div class="col-6">
+    <div class="card user-message">
+      <div class="card-header">
+        <img src="https://www.citypng.com/public/uploads/preview/hd-man-user-illustration-icon-transparent-png-11640168385tqosatnrny.png" alt="User" width="30" height="30" class="border border-2 rounded-circle">
+        ME
+      </div>
+      <div class="card-body bg-dark">
+        <blockquote class="blockquote mb-0">
+          <p>A well-known quote, contained in a blockquote element.</p>
+          <footer class="blockquote-footer user-footer">00/00/00 00:00</footer>
+        </blockquote>
+      </div>
+    </div>
+  </div>
+</div>
+`);
